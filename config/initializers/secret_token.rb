f352a55fd6dae8a8f8900542b53e839a0726e3af
Sam Wilks <sam.wilks@xtremelabs.com>
2013-01-19 19:21:25 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XtremeBieber::Application.config.secret_token = '39524b80fab951ba0fa94df701fcf5981dc3e5130702ef02eec303805679bab0d296f1169d28b7c2792eb084eae60734184843e74cad1d4f2d2a70f0afc52992'
